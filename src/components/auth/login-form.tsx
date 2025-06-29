ddac028f895465071a05c528d35639ee6c86dd83
Kuswara Senjaya <sejenakreda99@gmail.com>
2025-06-29 11:13:50 +0000

    masih: Terjadi Kesalahan Otentikasi dibutuhkan. Silakan login terlebih d

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -48,20 +48,6 @@ export function LoginForm() {
   async function onSubmit(values: z.infer<typeof formSchema>) {
     setIsLoading(true);
 
-    // Handle default admin user login
-    if (
-      values.email === "admin@datasiswa.com" &&
-      values.password === "admin123456"
-    ) {
-      toast({
-        title: "Login Berhasil",
-        description: "Selamat datang, Admin!",
-      });
-      router.push("/dashboard");
-      setIsLoading(false);
-      return;
-    }
-
     try {
       await signInWithEmailAndPassword(auth, values.email, values.password);
       toast({
